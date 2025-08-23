0f49d88a7d8bbb0dc3796d097e97fa92903f485c
Paulo Quirino <pauloqneto@gmail.com>
2025-08-23 11:27:52 -0300

    docs

--- manual.py
+++ manual.py
@@ -6,48 +6,48 @@ class ManualSistema(ft.Container):
         super().__init__()
         self.content = ft.Column(
             [
-                ft.Text("📘 Como usar o Sistema", size=22, weight="bold"),
+                ft.Text("📘 Como usar o Sistema", size=20, weight="bold"),
 
-                ft.Text("Visão Geral", size=20, weight="bold"),
+                ft.Text("Visão Geral", size=18, weight="bold"),
                 ft.Text(
                     "O Sistema de Recomendação analisa dados de estoque e notas fiscais "
-                    "para sugerir produtos durante uma venda de duas formas:\n\n"
+                    "para sugerir produtos durante uma venda de duas formas:\n"
                     "1. Substitutos → Sugere itens similares da mesma categoria quando "
                     "um produto não está disponível ou para oferecer alternativas de preço e margem.\n"
-                    "2. Associados → Indica produtos frequentemente comprados juntos.\n\n"
+                    "2. Associados → Indica produtos frequentemente comprados juntos.\n"
                     "O objetivo é aumentar o ticket médio de vendas com sugestões inteligentes e relevantes.",
                     text_align=ft.TextAlign.JUSTIFY,
-                    size=16
+                    size=14
                 ),
 
-                ft.Text("🔍 Como Funciona", size=20, weight="bold"),
+                ft.Text("🔍 Como Funciona", size=18, weight="bold"),
                 ft.Text(
                     "- Digite um código de produto no campo de pesquisa.\n"
                     "- Clique no botão pesquisar.\n"
                     "- O sistema irá mostrar as informações do produto pesquisado\n." \
                     "  Se o código for inválido, uma mensagem de erro será exibida.\n" \
                     "- Caso o produto seja encontrado, serão exibidos:\n    " \
-                    "- duas tabelas:\n\n" \
+                    "- duas tabelas:\n" \
                     "      1. Substitutos → Itens similares com informações de preço e margem.\n"
                     '      2. Associados → Produtos frequentemente comprados juntos.\n',
                     text_align=ft.TextAlign.JUSTIFY,
-                    size=16
+                    size=14
                 ),
 
-                ft.Text("💡 Dicas", size=20, weight="bold"),
+                ft.Text("💡 Dicas", size=18, weight="bold"),
                 ft.Text(
                     "- Use códigos completos para maior precisão.\n"
                     "- Verifique substitutos para opções alternativas.\n"
                     "- Explore os produtos vendidos juntos para aumentar as vendas.",
                     text_align=ft.TextAlign.JUSTIFY,
-                    size=16
+                    size=14
                 ),
             ],
             scroll=ft.ScrollMode.AUTO,
             spacing=10,
         )
 
-        self.padding = 22
+        self.padding = 12
         self.border_radius = 30
         self.bgcolor = ft.Colors.WHITE38
         self.expand = True
